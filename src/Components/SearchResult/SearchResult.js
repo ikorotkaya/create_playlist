8815ba0843ace9459fafe17d68899a798a850de8
ikorotkaya <irinkakorotkaya@gmail.com>
2023-01-16 18:08:12 +0100

    Initial commit

---
+++ src/Components/SearchResult/SearchResult.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './SearchResult.css';
+import TrackList from './Tracklist.js'
+
+class SearchResult extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        {/* <!-- Add a TrackList component --> */}
+      </div>
+    )
+  }
+}
+
+export default SearchResult
