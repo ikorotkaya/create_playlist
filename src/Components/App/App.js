d4ad6de9126b4852618d52eb3476c8def38cd45f
ikorotkaya <irinkakorotkaya@gmail.com>
2023-01-17 21:12:41 +0100

    Pass the playlist name and tracks from the App component to the Playlist component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,10 +8,15 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 'id1'}, 
-      {name: 'name2', artist: 'artist2', album: 'album2', id: 'id2'},
-      {name: 'name3', artist: 'artist3', album: 'album3', id: 'id3'}
-    ]
+      searchResults: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 1 },
+      { name: 'name2', artist: 'artist2', album: 'album2', id: 2 },
+      { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }
+      ],
+      playlistName: "My Playlist",
+      playlistTracks: [{ name: 'playlistName1', artist: 'PlaylistArtist1', album: 'playlistAlbum1', id: 1 },
+      { name: 'playlistName2', artist: 'PlaylistArtist2', album: 'playlistAlbum2', id: 2 },
+      { name: 'playlistName3', artist: 'PlaylistArtist3', album: 'playlistAlbum3', id: 3 }
+      ]
     }
   }
 
@@ -22,8 +27,9 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResult searchResults={this.state.searchResults}/>
-            <Playlist />
+            <SearchResult searchResults={this.state.searchResults} />
+            <Playlist playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
