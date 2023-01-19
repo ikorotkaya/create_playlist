a39b054fb9f2a9b8dc504133219001a055c964dc
ikorotkaya <irinkakorotkaya@gmail.com>
2023-01-19 12:37:13 +0100

    Create a method that updates the searchResults parameter in the App component with a user’s search results

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -22,6 +22,8 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+
   }
 
 
@@ -49,12 +51,16 @@ class App extends React.Component {
     const trackUris = this.state.playlistTracks.map(track => track.uri)
   }
 
+  search(searchTerm) {
+    console.log(searchTerm);
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search}/>
           <div className="App-playlist">
             <SearchResult searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName}
